0cfec6041ee914c48e118f2c0143d0a18b55a6ef
murkl <murkl@users.noreply.github.com>
2023-04-03 14:51:30 +0200

    alignment, rm package.sh

--- package.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-BUILD_BASE="$PWD"
-cd "${BUILD_BASE}/src"
-mkdir -p "${BUILD_BASE}/release"
-tar -czf "${BUILD_BASE}/release/plymouth-theme-arch-elegant.tar.gz" ./*
